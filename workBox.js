0a4990f28a9f68a00d55c78f116f61afd471c94f
johnotu <johnne01@gmail.com>
2015-06-24 17:09:08 +0100

    added decimal and float acceptance

--- workBox.js
+++ workBox.js
@@ -1,8 +1,3 @@
-/* Next Steps
-*** place a limit on the number of digits
-*** round up last allowed digit
-*** html/js navigation bar. */
-
 $(document).ready(function(){
 	var num = "";
 	var firstnum = "";
@@ -12,7 +7,7 @@ $(document).ready(function(){
 
 	$("#total").text("0");
 
-	$("#numbers a").not("#clear").click(function(){
+	$("#numbers a").not("#clear,#deci").click(function(){
 		num += $(this).text();
 		dispAr.push($(this).text());
 		$("#inline").text(dispAr.join(""));
@@ -25,7 +20,25 @@ $(document).ready(function(){
 		op = $(this).text();
 		firstnum = num;
 		num = "";
-	})
+	});
+
+	$("#deci").click(function(){
+		var numdeci = 0;
+		for(i=0; i<num.length; i++) {
+			numdeci++
+		}
+		if(numdeci>0) {
+			num += $(this).text();
+			dispAr.push($(this).text());
+			$("#inline").text(dispAr.join(""));
+			numlimit();
+		}
+		else {
+			num += "0.";
+			dispAr.push("0.");
+			$("#inline").text(dispAr.join(""));
+		};
+    });
 
 	$("#compute").click(function(){
 		$("#debug-display1").text(firstnum);
@@ -33,22 +46,24 @@ $(document).ready(function(){
 		$("#debug-display3").text(num);
 
 		if (op === "+") {
-			num = (parseInt(firstnum,10) + parseInt(num,10)).toString(10);
+			num = (parseFloat(firstnum,10) + parseFloat(num,10)).toString(10);
 		}
 		else if (op === "-") {
-			num = (parseInt(firstnum,10) - parseInt(num,10)).toString(10);
+			num = (parseFloat(firstnum,10) - parseFloat(num,10)).toString(10);
 		}
 		else if (op === "*") {
-			num = (parseInt(firstnum,10) * parseInt(num,10)).toString(10);
+			num = (parseFloat(firstnum,10) * parseFloat(num,10)).toString(10);
 		}
 		else if (op === "/") {
-			num = (parseInt(firstnum,10) / parseInt(num,10)).toString(10);
+			num = (parseFloat(firstnum,10) / parseFloat(num,10)).toString(10);
 		}
-
-		$("#total").text(num);
+		
+		$("#debug-display4").text(num);
+		
+		$("#total").text((numround(num)).toString(10));
 		num = "";
 		dispAr = [];
-		numlimit ();
+		numlimit();
 
 
 
@@ -66,6 +81,15 @@ $(document).ready(function(){
 			$("#total").text("err");
 			dispAr = [];
 			num = "";
+		}
+	};
+
+	var numround = function(num){
+		if (num.length>9) {
+			return num.substr(0,9);
+		}
+		else {
+			return num;
 		}
 	};
 
